7dbd9b6ecfa592f011e667b959a8c82658ddec5d
pridhvi <pridhvimuthuraju@gmail.com>
2023-03-27 14:46:40 -0400

    Updated like button to unlike

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -6,16 +6,29 @@ import { updateTuitThunk } from '../../services/tuits-thunks'
 const TuitStats = ({ tuit }) => {
 
     const dispatch = useDispatch()
+    const handleClickLike = () => {
+        if (tuit.liked) {
+            return dispatch(updateTuitThunk({
+                ...tuit,
+                likes: tuit.likes - 1,
+                liked: false
+            }))
+        }
+        else {
+            return dispatch(updateTuitThunk({
+                ...tuit,
+                likes: tuit.likes + 1,
+                liked: true
+            }))
+        }
+    }
+    
     return (
         <div className="wd-icons">
             <Link to="#"><i className="far fa-comment"></i><span>{tuit.replies}</span></Link>
             <Link to="#"><i className="fas fa-retweet"></i><span>{tuit.retuits}</span></Link>
             <Link to="#">
-                <i onClick={() => dispatch(updateTuitThunk({
-                    ...tuit,
-                    likes: tuit.likes + 1,
-                    liked: true
-                }))}
+                <i onClick={handleClickLike}
                     className={`${tuit.liked ? 'fas text-danger' : 'far'} fa-heart`}></i>
                 <span>{tuit.likes}</span>
             </Link>
